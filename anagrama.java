8ab38153b37f44d566a972dbe678d7b3c664bf67
crstian <cdarenizc@ufpso.edu.co>
2024-10-04 17:19:56 -0500

    primer commit

---
+++ anagrama.java
@@ -0,0 +1,11 @@
+public class anagrama {
+    public static void main(String[] args) {
+        char [] array_word= {'A','M','O','R'};
+        char [] array_anagrama={'R','O','M','A'};
+
+
+        
+
+
+    }
+}
